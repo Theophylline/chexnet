9c86202d36254a7dbbde0d8183919d64da41941d
Mike Qiu <mike.qiu@gmail.com>
2018-05-13 20:47:13 -0400

    transition layer

--- model.py
+++ model.py
@@ -1,2 +1,25 @@
-# -*- coding: utf-8 -*-
+import tensorflow as tf
+import glob
 
+TFRecords_dir = ""
+fpaths = glob.glob(TFRecords_dir + "*.tfrecords")
+
+#%%
+
+# read from TFRecords
+ds = tf.data.TFRecordDataset(fpaths)
+# model
+
+def DenseNet(batch_img, depth = 121, k = 12):
+    
+    # dense block 1
+    
+    #transition layer
+    
+    # dense block 2
+    
+    # ...
+    
+    # classification (global max pooling and softmax)
+    
+    return None
